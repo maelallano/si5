1c942a95b1b44ed366048cf241083a977d9dbd5d
root <anonhq789@protonmail.com>
2018-04-10 15:19:46 +0200

    :art: Admin assets and views

---
+++ src/views/admin/header.inc.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en-us">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title><?=SITE_NAME?></title>
+    <link rel="shortcut icon" href="./favicon.ico">
+    <link rel="stylesheet" href="<?=WEBROOT?>build/app.css">
+</head>
+<body>
