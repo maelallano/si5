3584c35cacbe09e3416eb27b3fd4cc8248df0fb2
mallano <maelallano1@gmail.com>
2018-04-10 16:36:02 +0200

    loader_v1

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,9 @@
 import '../scss/common.scss';
 
 var gameInstance = UnityLoader.instantiate("gameContainer", "Unity/build_web.json", { onProgress: UnityProgress });
+
+var loaderContainer = document.querySelector('#loaderContainer');
+
+setTimeout(() => {
+	loaderContainer.style.display = 'none';
+}, 3000);
